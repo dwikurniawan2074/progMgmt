1878feba0f83570e1ca785dd1c3a03ea7b4d59e1
dwikurniawan2074 <dwi.kurniawan2074@students.unila.ac.id>
2023-08-26 12:41:25 +0700

    tambah project template

---
+++ app/Views/template/footer.php
@@ -0,0 +1,16 @@
+<div class="py-6 px-6 text-center">
+      <p class="mb-0 fs-4">Project made by <a href="https://github.com/dwikurniawan2074/projectManagement.git" target="_blank" class="pe-1 text-primary text-decoration-underline">Kelompok 5 MSIB Trafindo</a></p>
+    </div>
+  </div>
+</div>
+</div>
+<script src="<?php echo base_url() ?>modernize/src/assets/libs/jquery/dist/jquery.min.js"></script>
+<script src="<?php echo base_url() ?>modernize/src/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo base_url() ?>modernize/src/assets/js/sidebarmenu.js"></script>
+<script src="<?php echo base_url() ?>modernize/src/assets/js/app.min.js"></script>
+<script src="<?php echo base_url() ?>modernize/src/assets/libs/apexcharts/dist/apexcharts.min.js"></script>
+<script src="<?php echo base_url() ?>modernize/src/assets/libs/simplebar/dist/simplebar.js"></script>
+<script src="<?php echo base_url() ?>modernize/src/assets/js/dashboard.js"></script>
+</body>
+
+</html>
