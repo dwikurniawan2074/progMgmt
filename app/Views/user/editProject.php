1598a7f905a76dde1b27d8e71284423948f5b732
petersonhz25 <82883040+zehamless@users.noreply.github.com>
2023-08-27 13:21:36 +0700

    fix: project crud and view

--- app/Views/user/editProject.php
+++ app/Views/user/editProject.php
@@ -8,34 +8,38 @@
                 <h5 class="card-title fw-semibold mb-4">Edit Project</h5>
                 <div class="card">
                     <div class="card-body">
-                        <form action="<?= base_url(); ?>user/index" method="GET">
+                        <form action="<?= site_url('/user/editProject/update/' . $projects['id']) ?>" method="post">
                             <div class="mb-3">
                                 <label class="form-label">Nama</label>
-                                <input type="text" class="form-control">
+                                <input type="text" class="form-control" name="nama" value="<?=esc($projects['name']) ?>">
                             </div>
                             <div class="mb-3">
                                 <label class="form-label">Deskripsi</label>
-                                <textarea class="form-control"></textarea>
+                                <textarea class="form-control" name="deskripsi">
+                                    <?=esc($projects['deskripsi']) ?>
+                                </textarea>
                             </div>
                             <div class="mb-3">
                                 <label class="form-label">Anggota Tim</label>
-                                <textarea class="form-control"></textarea>
+                                <textarea class="form-control" name="anggota_tim">
+                                    <?=esc($projects['anggota_tim']) ?>
+                                </textarea>
                             </div>
-                            <div class="mb-3">
-                                <label class="form-label">Tanggal Input</label>
-                                <input type="text" class="form-control">
-                            </div>
+<!--                            <div class="mb-3">-->
+<!--                                <label class="form-label">Tanggal Input</label>-->
+<!--                                <input type="text" class="form-control">-->
+<!--                            </div>-->
                             <div class="mb-3">
                                 <label class="form-label">Tanggal Deadline</label>
-                                <input type="text" class="form-control">
+                                <input type="date" class="form-control" name="tanggal_deadline" value="<?= esc($projects['tanggal_deadline']) ?>
                             </div>
-                            <div class="mb-3">
-                                <label class="form-label">Status</label>
-                                <select class="form-select">
-                                    <option value="1">On-Progress</option>
-                                    <option value="2">Done</option>
-                                </select>
-                            </div>
+<!--                            <div class="mb-3">-->
+<!--                                <label class="form-label">Status</label>-->
+<!--                                <select class="form-select">-->
+<!--                                    <option value="1">On-Progress</option>-->
+<!--                                    <option value="2">Done</option>-->
+<!--                                </select>-->
+<!--                            </div>-->
                             <a href="<?= base_url(); ?>user/index">
                                 <button type="button" class="btn btn-danger">Back</button>
                             </a>
